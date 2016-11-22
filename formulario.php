6e1fb05a250370d1edbddbc2334f6b9677cdbeeb
alfredo322 <nobrain173@gmail.com>
2016-11-21 20:53:54 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,60 @@
+<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Form Name</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Nombres">Nombres:</label>  
+  <div class="col-md-4">
+  <input id="Nombres" name="Nombres" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Apellidos">Apellidos</label>  
+  <div class="col-md-4">
+  <input id="Apellidos" name="Apellidos" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Edad">Edad:</label>  
+  <div class="col-md-4">
+  <input id="Edad" name="Edad" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Fecha_nacimiento">Fecha de Nacimiento</label>  
+  <div class="col-md-4">
+  <input id="Fecha_nacimiento" name="Fecha_nacimiento" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Textarea -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Celular">Ingrese sus celulares(1 por linea):</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="Celular" name="Celular"></textarea>
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Enviar"></label>
+  <div class="col-md-4">
+    <button id="Enviar" name="Enviar" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
